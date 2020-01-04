4e008bdc14c8a33c99de24a94a0bfb853f1422b8
Nall-chan <mt@neo-ami.de>
2020-01-04 21:08:12 +0100

    Fix Readme

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
+[![Version](https://img.shields.io/badge/Symcon-PHPModul-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
+[![Version](https://img.shields.io/badge/Modul%20Version-0.50-blue.svg)]()
+[![Version](https://img.shields.io/badge/Symcon%20Version-5.3%20%3E-green.svg)](https://www.symcon.de/forum/threads/30857-IP-Symcon-5-3-%28Stable%29-Changelog)  
+[![License](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-green.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)  
+[![Check Style](https://github.com/Nall-chan/ServerWMC/workflows/Check%20Style/badge.svg)](https://github.com/Nall-chan/ServerWMC/actions) [![Run Tests](https://github.com/Nall-chan/ServerWMC/workflows/Run%20Tests/badge.svg)](https://github.com/Nall-chan/ServerWMC/actions)  
 # ServerWMC
 
 Folgende Module beinhaltet das ServerWMC Repository:
 
-- __ServerWMC IO__ ([Dokumentation](ServerWMC IO))
+- __ServerWMC IO__ ([Dokumentation](ServerWMC%20IO))
 	Kurze Beschreibung des Moduls.
 
-- __ServerWMC Timers__ ([Dokumentation](ServerWMC Timers))
+- __ServerWMC Timers__ ([Dokumentation](ServerWMC%20Timers))
 	Kurze Beschreibung des Moduls.
 
-- __ServerWMC SeriesTimers__ ([Dokumentation](ServerWMC SeriesTimers))
+- __ServerWMC SeriesTimers__ ([Dokumentation](ServerWMC%20SeriesTimers))
 	Kurze Beschreibung des Moduls.
 
-- __ServerWMC Recordings__ ([Dokumentation](ServerWMC Recordings))
+- __ServerWMC Recordings__ ([Dokumentation](ServerWMC%20Recordings))
 	Kurze Beschreibung des Moduls.
 
-- __ServerWMC Channels__ ([Dokumentation](ServerWMC Channels))
+- __ServerWMC Channels__ ([Dokumentation](ServerWMC%20Channels))
 	Kurze Beschreibung des Moduls.
--- tests/IOTest.php
+++ tests/IOTest.php
@@ -10,40 +10,47 @@ use PHPUnit\Framework\TestCase;
 
 class IOTest extends TestCase
 {
-    private $IoID = '{7681F2B3-FA3A-D6A1-F890-DAE6E3E9AFB3}';
-    public function setUp(): void
+    private static $IoID = '{7681F2B3-FA3A-D6A1-F890-DAE6E3E9AFB3}';
+    private static $InstanceId = 0;
+
+    public static function setUpBeforeClass(): void
     {
         //Reset
         IPS\Kernel::reset();
         //Register our library we need for testing
         IPS\ModuleLoader::loadLibrary(__DIR__ . '/../library.json');
-        parent::setUp();
-    }
 
-    public function testGetChannels()
-    {
-        $id = IPS_CreateInstance($this->IoID);
+        $id = IPS_CreateInstance(self::$IoID);
         IPS_SetProperty($id, 'Open', true);
         if (isset($_ENV['COMPUTERNAME'])) {
-            IPS_SetProperty($id, 'Host', '127.0.0.1');
+            IPS_SetProperty($id, 'Host', '192.168.201.253');
             IPS_ApplyChanges($id);
         } else {
             IPS_SetProperty($id, 'Host', '127.0.0.1');
             @IPS_ApplyChanges($id);
         }
+        self::$InstanceId = $id;
+    }
 
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testGetChannels()
+    {
         $json = json_encode(
             [
                 'Function'=> 'GetChannels',
                 'Params'  => []
             ]
         );
-        $Channels = IPS\InstanceManager::getInstanceInterface($id)->ForwardData($json);
+        $Channels = IPS\InstanceManager::getInstanceInterface(self::$InstanceId)->ForwardData($json);
         if (is_bool($Channels)) {
             $this->assertFalse($Channels);
-            $this->
         } else {
             $this->assertGreaterThan(0, count(unserialize($Channels)));
+            print_r(count(unserialize($Channels)));
         }
     }
 }
